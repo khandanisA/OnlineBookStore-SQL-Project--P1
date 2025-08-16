f67b1329c28ab938f737beb433d624056183aee4
MD DANISH KHAN <khandanishg@gmail.com>
2025-08-16 08:03:05 +0530

    Update SOLVED.sql

--- SOLVED.sql
+++ SOLVED.sql
@@ -1,1 +1,41 @@
+--Create Database
+create database OnlineBookStoreDB;
 
+--Create Tables
+drop table if exists books;
+
+create table books(Book_ID serial primary key,
+                   Title varchar(150) not null,
+				   Author varchar(150) not null,
+				   Genre varchar(150) not null,
+				   Published_Year int not null,
+				   Price numeric not null,
+				   Stock int not null
+				   );
+
+drop table if exists customers;	
+create table customers(Customer_ID serial primary key,
+                       Name varchar(100) not null,
+					   Email varchar(200) not null,
+					   Phone int,
+					   City varchar(100) not null,
+					   Country varchar(100) not null
+					   ); 
+
+drop table if exists orders;
+create table orders(Order_ID serial primary key,
+                    Customer_ID int not null,
+					Book_ID int not null,
+					Order_Date date not null,
+					Quantity int not null,
+					Total_Amount numeric,
+					foreign key(customer_id) references customers(customer_id),
+					foreign key(book_id) references books(book_id)
+                   );
+
+--Import Data into Books table				   
+copy
+customers(Customer_ID,Name,Email,Phone,City,Country)
+from '‪C:\Users\MD DANISH KHAN\OneDrive\Desktop\Customers.csv'
+delimiter','
+csv header;
