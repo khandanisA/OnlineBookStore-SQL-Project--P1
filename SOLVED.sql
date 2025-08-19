b8454ab4e0f9ea83754cbe0961141dda2ff34536
MD DANISH KHAN <khandanishg@gmail.com>
2025-08-19 16:03:17 +0530

    Update SOLVED.sql

--- SOLVED.sql
+++ SOLVED.sql
@@ -119,4 +119,45 @@ JOIN customers c ON o.customer_id=c.customer_id
 GROUP BY o.customer_id, c.name
 HAVING COUNT(O.Order_id) >=2;
 
+--4) Find the most frequently ordered book
+select o.book_id,
+       b.title,
+	   count(o.order_id) as orders_count
+from orders o join books b 
+on o.book_id=b.book_id
+group by o.book_id,b.title
+order by orders_count desc
+limit 1;
+
+--5) Show the top 3 most expensive books of 'Fantasy' Genre 
+select * from books
+where genre='Fantasy'
+order by price desc
+limit 3;
+
+--6) Retrieve the total quantity of books sold by each author
+select b.author,
+       sum(quantity) as total_quantity_of_books_sold
+from books b
+join orders o on b.book_id=o.book_id
+group by b.author
+order by total_quantity_of_books_sold;
+
+--7) List the cities where customers who spent over $30 are located
+select distinct(c.city),o.total_amount from customers c
+join orders o on c.customer_id=o.customer_id
+where o.total_amount > 30
+order by total_amount;
+
+--8) Find the customer who spent the most on orders
+select o.customer_id,
+       c.name,
+       sum(o.total_amount) as most_spent_customer
+from orders o join customers c on o.customer_id=c.customer_id
+group by o.customer_id,c.name
+order by most_spent_customer desc 
+limit 1;
+
+
+
 
